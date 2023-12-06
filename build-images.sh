c5b82a829ee038fdfa50f187ed90794bd56d1744
rster2002 <rster2002dev@gmail.com>
2023-12-06 17:32:28 +0100

    Some "progress"

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,12 @@
+echo tag:
+read -r TAG
+
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t docker.jumpdrive.dev/discord-bot-application:latest \
+  -t docker.jumpdrive.dev/discord-bot-application:$TAG \
   --push ./DiscordbotTest7
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t docker.jumpdrive.dev/discord-bot-lavalink:latest \
+  -t docker.jumpdrive.dev/discord-bot-lavalink:$TAG \
   --push ./lavalink
